990c4d70673cb50bbf03ce7d027ba78246f88919
Akhileswar <akhileswarsathivada@gmail.com>
2024-02-16 07:01:33 +0530

    16\02\2024

--- src/components/companyReg/RegisterOrg.jsx
+++
@@ -1,6 +0,0 @@
-import styles from "./RegisterOrg.module.css";
-const RegisterOrg = () => {
-  return <div>RegisterOrg</div>;
-};
-
-export default RegisterOrg;
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,31 +1,43 @@
-import styles from './register.module.css';
-import {Link} from 'react-router-dom'
+import styles from "./register.module.css";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
-    <div className={styles.container} >
-      
+    <div className={styles.container}>
       <div className={styles.box}>
         <div className={styles.left}>
           <h1>Sign Up</h1>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero cumque tempora unde odio eveniet, tenetur voluptate nostrum nam harum veritatis fuga eaque hic maiores? Adipisci fugiat, iusto corporis, eligendi voluptatem amet voluptate perspiciatis assumenda sit, reprehenderit soluta eius sequi! Quisquam placeat earum ipsa quia officia molestias voluptatem, accusamus ut ea.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
+            cumque tempora unde odio eveniet, tenetur voluptate nostrum nam
+            harum veritatis fuga eaque hic maiores? Adipisci fugiat, iusto
+            corporis, eligendi voluptatem amet voluptate perspiciatis assumenda
+            sit, reprehenderit soluta eius sequi! Quisquam placeat earum ipsa
+            quia officia molestias voluptatem, accusamus ut ea.
+          </p>
         </div>
         <div className={styles.right}>
           <div className={styles.formBox}>
-          <img src="https://i.pinimg.com/564x/9d/3a/a1/9d3aa1c2648372c8e75dc3d79f06f67f.jpg" alt="" />
-          <form>
-            <input type="email" placeholder='Email'/>
-            <input type="password" placeholder='Password'/>
-            <button>Login</button>
-          </form>
+            <img
+              src="https://github.com/Akhileswar1905/Alliance/blob/main/public/assets/lock.png"
+              alt=""
+            />
+            <form>
+              <input type="text" placeholder="Organization Name" />
+              <input type="number" placeholder="Year of Establishment" />
+              <input type="text" placeholder="Domain of the Organizationn" />
+              <input type="text" placeholder="State, Country" />
+              <textarea placeholder="Description of the Organization" />
+              <button>Sign Up</button>
+            </form>
           </div>
           <hr />
           <p>
-            Don't have an account <Link to={"/login"}>Click Here</Link>
+            Don&apos;t have an account <Link to={"/login"}>Click Here</Link>
           </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
